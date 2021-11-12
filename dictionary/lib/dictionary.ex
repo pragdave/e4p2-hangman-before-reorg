41d2f345684f981c79a7411049a4354912885de5
pragdave <dave@pragdave.me>
2021-11-12 13:50:27 -0600

    Initial load for course

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,11 @@
+defmodule Dictionary do
+
+@word_list "assets/words.txt"
+  |> File.read!()
+  |> String.split(~r/\n/, trim: true)
+
+  def random_word do
+    @word_list
+    |> Enum.random()
+  end
+end
