ee4e1161d50a457708e092cb2011d7c0ae820acc
pragdave <dave@pragdave.me>
2021-11-12 15:25:19 -0600

    Initial load for course

---
+++ b2/config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :b2, B2Web.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "MVcsvBKdlY1XmzT7dv+aIGeYxQ243EOv1xbt34c9cfqKu7IN/1JyWZ2feykr7OzU",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ b2/lib/b2_web/live/game/alphabet.ex
@@ -0,0 +1,32 @@
+
+defmodule B2Web.Live.Game.Alphabet do
+
+  use B2Web, :live_component
+
+  def mount(socket) do
+    letters = (?a..?z) |> Enum.map(&<< &1 :: utf8 >>)
+    { :ok, assign(socket, :letters, letters) }
+  end
+
+  def render(assigns) do
+    ~L"""
+    <div class="alphabet">
+    <%= for letter <- assigns.letters do %>
+      <div phx-click="make_move"
+           phx-value-key="<%= letter %>"
+           class="one-letter <%= classOf(letter, @tally) %>">
+      <%= letter %>
+      </div>
+      <% end %>
+    </div>
+    """
+  end
+
+  defp classOf(letter, tally) do 
+    cond do
+      Enum.member?(tally.letters, letter) -> "correct"
+      Enum.member?(tally.used, letter)    -> "wrong"
+      true -> ""
+    end
+  end
+end
