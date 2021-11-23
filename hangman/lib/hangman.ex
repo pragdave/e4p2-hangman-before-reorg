a76d357a1318910144afbbf1973d24d48fa5561e
pragdave <dave@pragdave.me>
2021-11-23 21:01:55 +0000

    add intermediate step

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,9 +1,10 @@
 defmodule Hangman do
 
-  alias Hangman.Impl.Game
+  alias Hangman.Runtime.Server
 
   @type   state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
-  @opaque game  :: Game.t
+  @opaque game  :: Server.t
+
   @type   tally :: %{
     turns_left: integer,
     game_state: state,
@@ -12,11 +13,18 @@ defmodule Hangman do
   }
 
   @spec new_game() ::  game
-  defdelegate new_game, to: Game
+  def new_game() do 
+    { :ok, pid } = Server.start_link
+    pid
+  end
 
   @spec make_move(game, String.t) :: { game, tally }
-  defdelegate make_move(game, guess), to: Game
+  def make_move(game, guess) do
+    GenServer.call(game, { :make_move, guess })
+  end
 
   @spec tally(game) :: tally()
-  defdelegate tally(game), to: Game
+  def tally(game) do
+    GenServer.call(game, { :tally })
+  end
 end
